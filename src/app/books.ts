73a9a8f6cf82ae857f2c1c85d18bc53b0456fb78
Yassir Morris <yassir.morris.120@gmail.com>
2022-08-29 14:34:29 +0800

    Added bookmarking current page

---
+++ src/app/books.ts
@@ -0,0 +1,8 @@
+import { AvailableBookType } from "./types";
+
+export const AvailableBooks: AvailableBookType[] = [
+    {
+        name: "The Heroes",
+        path: "https://yassirmorris.github.io/reader/files/The Heroes.epub"
+    }
+]
---
+++ src/app/types.ts
@@ -0,0 +1,4 @@
+export type AvailableBookType = {
+    name: string,
+    path: string
+}
