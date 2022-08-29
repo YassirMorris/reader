04e1868d9db4cdb7c61da97f92e2698facffa004
Yassir Morris <yassir.morris.120@gmail.com>
2022-08-29 15:34:59 +0800

    Revert "Added initial routing"
    This reverts commit 81393770b43bbbae41ab021a843734256c3a7b1c.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.scss';
-import Disclaimer from './pages/Disclaimer/Disclaimer';
-import Home from './pages/Home/Home';
+import { ReactEpubViewer } from 'react-epub-viewer';
+import BookType from 'react-epub-viewer/lib/types/book'
+import Page from 'react-epub-viewer/lib/types/page'
+import { AvailableBooks } from './app/books';
 
 function App() {
+  const [books, setBooks] = useState(AvailableBooks);
+  const [locations, setLocations] = useState<string[]>([''])
+  const [curretBookDetails, setCurrentBookDetails] = useState<BookType>();
+  const [curretBook, setCurrentBook] = useState(books[0]);
+  const viewerRef = useRef<any>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLocations(JSON.parse(localStorage.getItem(Object.keys(localStorage).find(key => key.includes('locations'))!)!))
+    }, 0)
+  }, [])
+
+  useEffect(() => {
+    setTimeout(()=>{
+      if(curretBookDetails && viewerRef.current){
+        viewerRef.current.setLocation(locations[Number(localStorage.getItem('currentPage'))])
+      }
+    }, 150)
+  }, [curretBookDetails])
+
+
+  const onBookInfoChange = (book: BookType) => {
+    setCurrentBookDetails(book);
+  }
+
+  const onPageChange = (page: Page) => {
+    if(page.currentPage !== 0){
+      localStorage.setItem('currentPage', page.currentPage.toString())
+    }
+  }
+
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/disclaimer' element={<Disclaimer/>}/>
-      </Routes>
+      <div className="Body">
+        <ReactEpubViewer
+          url={curretBook.path}
+          ref={viewerRef}
+          onBookInfoChange={onBookInfoChange}
+          onPageChange={onPageChange}
+        />
+      </div>
     </div>
   );
 }
