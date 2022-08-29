2046ecc8c9c40ee7e71942b530729d17a9a4d19f
Yassir Morris <yassir.morris.120@gmail.com>
2022-08-29 12:31:43 +0800

    Updated path of first book

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <div className="Body">
         <ReactEpubViewer 
-          url={'reader/files/The Heroes.epub'}
+          url={'https://yassirmorris.github.io/reader/files/The Heroes.epub'}
           ref={viewerRef}
         />
       </div>
